873f44a801cfd9da382e8f68223d5c0445e804d1
YourFullName <sashakashi@mail.ru>
2022-09-15 14:16:34 +0200

    upd

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,13 +1,42 @@
 package entities;
+import lombok.Data;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
+@Data
 public class User {
     private int id;
     private String firstName;
     private String lastName;
     private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
